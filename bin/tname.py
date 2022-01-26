7cfb2f506be10a7b0bc445b11d2167b13965de14
enovillo-shopify <ernesto.novillo@shopify.com>
2022-01-26 15:44:32 -0500

    v0.9 is ready.

--- bin/tname.py
+++ bin/tname.py
@@ -7,26 +7,23 @@ import sys
 
 
 def main(args):
-
   if len(args) >= 1:
-    test = args[0] #"../../Shopify/shopify/**/inventory_transfers_controller_test.rb:26"
-    print(args)
-    test_ids = test.split(":")
+    test_ids = args[0].split(":")
 
     if len(test_ids) != 2:
-      print(f"Bad test spec: {test}")
+      print(f"Bad test spec: {args[0]}")
 
     else:
-      fnames = get_files(test_ids[0])
+      fnames =glob.glob(f"**/{test_ids[0]}_test.rb", recursive = True)
       if len(fnames) == 0:
-        print(f"Test not found: {test}")
+        print(f"Test not found: {test_ids[0]}")
 
       elif len(fnames) == 1:
         line = linecache.getline(fnames[0], int(test_ids[1]))
         m = re.search("test \"(?P<desc>.*)\" do", line)
         if m:
           test_name = f"test_{re.sub(' ', '_', m.group('desc'))}"
-          print(test_name)
+          print(f"{fnames[0]} -n {test_name}")
         else:
           print(f"No test description found in line {test_ids[1]}:\n'{line[:-1]}'")
 
@@ -36,10 +33,6 @@ def main(args):
   else:
     print("No test file given.")
 
-
-
-def get_files(name):
-  return glob.glob(name, recursive = True)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
